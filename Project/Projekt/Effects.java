1a69e8808d2274cdf70318bf968815bdf555606c
CoStellar <tatrenings@gmail.com>
2022-05-14 20:53:15 +0200

    Początkowa wersja Projektu

--- Project/Projekt/Effects.java
+++ Project/Projekt/Effects.java
@@ -1,4 +1,23 @@
 package Project.Projekt;
 
 public class Effects {
+    private Position effectPosition;
+    private Boolean effectActive;
+    private Integer effectFinishTime;
+
+    Effects(Position position){
+
+    }
+    void breakage(){
+    }
+    void depopulation(){
+    }
+    void mobilityReduction(){
+    }
+    void mobilityRestriction(){
+    }
+    void resourcesGatherReduction(){
+    }
+    void destruction(){
+    }
 }
--- Project/Projekt/Resources.java
+++ Project/Projekt/Resources.java
@@ -1,4 +1,22 @@
 package Project.Projekt;
-
+import java.util.Random;
 public class Resources {
+    Integer wood;
+    Integer iron;
+    Integer wheat;
+    Integer animals;
+    Integer stone;
+    Integer water;
+    Random randomAmount = new Random();
+    Resources(){
+        this.wood = randomAmount.nextInt(3);
+        this.iron = randomAmount.nextInt(3);
+        this.wheat = randomAmount.nextInt(3);
+        this.animals = randomAmount.nextInt(3);
+        this.stone = randomAmount.nextInt(3);
+        this.water = randomAmount.nextInt(3);
+    }
+    void drawResources(){
+    }
+
 }
