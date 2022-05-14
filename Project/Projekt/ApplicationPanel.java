dc9215900ba3cd80f16b6a2480397e39f3baa1b6
CoStellar <tatrenings@gmail.com>
2022-05-14 19:22:13 +0200

    Początkowa wersja Projektu

---
+++ Project/Projekt/ApplicationPanel.java
@@ -0,0 +1,12 @@
+package Project.Projekt;
+
+import javax.swing.*;
+
+public class ApplicationPanel {
+
+        ApplicationPanel(){
+            JFrame frame = new JFrame();
+
+            frame.setVisible(True);
+        }
+}
--- Project/Projekt/Position.java
+++ Project/Projekt/Position.java
@@ -9,4 +9,4 @@ public class Position {
             this.y = y;
         }
 
-}
+}`
