4687bbab0fbb4313d1b5bb641904fcdaacc9e4b2
CoStellar <tatrenings@gmail.com>
2022-05-18 09:39:01 +0200

    Zaimplementowanie działającej losowo generowanej mapy symulacji

--- Project/Projekt/Resources.java
+++ Project/Projekt/Resources.java
@@ -4,62 +4,96 @@ import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.io.InputStream;
+import java.lang.constant.Constable;
+import java.nio.Buffer;
 import java.util.ArrayList;
+import java.util.Objects;
 import java.util.Random;
 
 public class Resources {
+    String resourceName;
     Integer wood;
     Integer iron;
     Integer wheat;
     Integer animals;
     Integer stone;
     Integer water;
+    private BufferedImage resourceImg;
 
-    private ArrayList<BufferedImage> resourceImg = new ArrayList<>();
 
-    private void importImg() {
-        InputStream is = getClass().getResourceAsStream("Pliki/wood_log_filled.png");
-        try {
-            resourceImg.set(0, ImageIO.read(is));
-        } catch (IOException e) {
-            e.printStackTrace();
+    public Resources(){
+        this.resourceName = drawResources();
+        importImg(this.resourceName);
+    }
+    public void importImg(String resourceName) {
+        InputStream is;
+        if(Objects.equals(resourceName, "wood")){
+            is = getClass().getResourceAsStream("Pliki/wood_log_filled.png");
+            try {
+                this.resourceImg = ImageIO.read(is);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }else if(Objects.equals(resourceName, "water")){
+            is = getClass().getResourceAsStream("Pliki/water_filled.png");
+            try {
+                this.resourceImg = ImageIO.read(is);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }else if(Objects.equals(resourceName, "cow")){
+            is = getClass().getResourceAsStream("Pliki/cow_filled.png");
+            try {
+                this.resourceImg = ImageIO.read(is);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }else if(Objects.equals(resourceName, "wheat")){
+            is = getClass().getResourceAsStream("Pliki/wheat_filled.png");
+            try {
+                this.resourceImg = ImageIO.read(is);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }else if(Objects.equals(resourceName, "iron")){
+            is = getClass().getResourceAsStream("Pliki/iron_filled.png");
+            try {
+                this.resourceImg = ImageIO.read(is);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }else if(Objects.equals(resourceName, "stone")){
+            is = getClass().getResourceAsStream("Pliki/stone_filled.png");
+            try {
+                this.resourceImg = ImageIO.read(is);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
-        is = getClass().getResourceAsStream("Pliki/water_filled.png");
-        try {
-            resourceImg.set(1, ImageIO.read(is));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        is = getClass().getResourceAsStream("Pliki/cow_filled.png");
-        try {
-            resourceImg.set(2, ImageIO.read(is));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        is = getClass().getResourceAsStream("Pliki/wheat_filled.png");
-        try {
-            resourceImg.set(3, ImageIO.read(is));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        is = getClass().getResourceAsStream("Pliki/iron_filled.png");
-        try {
-            resourceImg.set(4, ImageIO.read(is));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        is = getClass().getResourceAsStream("Pliki/stone_filled.png");
-        try {
-            resourceImg.set(5, ImageIO.read(is));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+
+    }
+    public BufferedImage getImg(){
+        return this.resourceImg;
     }
 
 
-    Random randomAmount = new Random();
-
-    void drawResources(){
+    String drawResources(){
+        int random;
+        random = (int) (Math.random() * 6);
+        if(random == 0)
+                return "wood";
+        if(random == 1)
+                return "water";
+        if(random == 2)
+                return "cow";
+        if(random == 3)
+                return "wheat";
+        if(random == 4)
+                return "iron";
+        if(random == 5)
+                return "stone";
+        else
+        return null;
     }
 
 }
