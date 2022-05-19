cd11e828bd9783556f5984d3a906fa0e1bf2fbe5
CoStellar <tatrenings@gmail.com>
2022-05-19 16:09:53 +0200

    Działający Maven

--- CivSim/src/main/java/com/civsim/CivilizationUnits.java
+++ CivSim/src/main/java/com/civsim/CivilizationUnits.java
@@ -2,7 +2,26 @@ package com.civsim;
 
 public class CivilizationUnits {
         String unitOwner;
+        Position[] traderUnitsPositions;
+        Position[] militaryUnitsPositions;
+        Integer militaryUnitsAmount;
+        Integer traderUnitsAmount;
 
+        public Integer totalUnitsAmount(){
+                return this.militaryUnitsAmount+this.traderUnitsAmount;
+        }
+        public Integer checkMilitaryAmount(){
+                return this.militaryUnitsAmount;
+        }
+        public Integer checkTraderAmount(){
+                return this.traderUnitsAmount;
+        }
+        public void updateTrader(Position[] positions, String unitOwner){
+
+        }
+        public void updateMilitary(Position[] positions, String unitOwner){
+
+        }
 
 
 }
